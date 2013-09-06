65da2c0dd81f494c6741d684766ceb431f4362a8
Scott Douglass <scott@scottyd.net>
2013-09-05 20:06:06 -0400

    Added actions object for publish post to fix for ember-data 1.0

--- tmp/app/app/modules/posts/controllers/new.js
+++ tmp/app/app/modules/posts/controllers/new.js
@@ -1,17 +1,24 @@
 Anthracite.PostsNewController = Ember.ArrayController.extend({
-	publishPost: function() {
-		var title = this.get('postTitle')
-		, postContent = this.get('postContent');
-		
-		if (!title.trim() || !postContent.trim()) { return; }
-		
-		var post = Anthracite.Post.createRecord({
-			postTitle: title,
-			postContent: postContent
-		});
-		
-		post.save();
-		
-		this.transitionToRoute('posts');
+	actions: {
+		publishPost: function() {
+			var postTitle = this.get('postTitle')
+			, postContent = this.get('postContent');
+			
+			if (!postTitle.trim() || !postContent.trim()) { return; }
+			
+//			var post = Anthracite.Post.createRecord({
+//				postTitle: postTitle,
+//				postContent: postContent
+//			});
+
+			var store = this.get('store');
+			store.push('post', {
+				id: 3,
+				postTitle: postTitle,
+				postContent: postContent
+			});
+			
+			this.transitionToRoute('posts');
+		}
 	}
 });
